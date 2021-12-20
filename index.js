93f06947defb5de642c632cffa9ec9e122c68973
César Daniel López Noblecias <danielnoblecias@MacBook-Air-de-Cesar.local>
2021-12-19 21:08:30 -0600

    [master] add some logs

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ route.post('/send-email', (req, res) => {
 });
 
 route.post('/verify-opt', (req, res) => {
+    console.log(req.cookies);
+    console.log(req.headers);
     res.status(200).send({
         echo: JSON.stringify(req.cookies)
     })
